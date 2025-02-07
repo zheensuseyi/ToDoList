e038aeba212ebb7e99e897f1d1893e5880fc1300
zheen <suseyizheen@gmail.com>
2025-02-06 17:46:05 -0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Zheen Suseyi on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
